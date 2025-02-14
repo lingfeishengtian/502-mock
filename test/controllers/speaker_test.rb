5754377042a02fa5a4c4dd79c87bc46aa7de1fc6
andycorrales111 <andycorrales@tamu.edu>
2025-02-14 18:28:10 +0000

    Changed to rspec tests

--- test/controllers/speaker_test.rb
+++ test/controllers/speaker_test.rb
@@ -1,25 +1,23 @@
-require "test_helper"
+require "rails_helper"
 
-class SpeakerTest < ActiveSupport::TestCase
-  test "should not save speaker without name" do
-    speaker = Speaker.new
-    speaker.email = "test@gmail.com"
-    assert_not speaker.save, "Saved the speaker without a name"
+RSpec.describe Speaker, type: :model do
+  it "is not valid without a name" do
+    speaker = Speaker.new(email: "test@gmail.com")
+    expect(speaker.save).to be_falsey, "Saved the speaker without a name"
   end
-  test "should not save speaker without email" do
-    speaker = Speaker.new
-    speaker.name = "Test, Test"
-    assert_not speaker.save, "Saved the speaker without an email"
+
+  it "is not valid without an email" do
+    speaker = Speaker.new(name: "Test, Test")
+    expect(speaker.save).to be_falsey, "Saved the speaker without an email"
   end
-  test "should not save speaker with invalid email" do
-    speaker = Speaker.new
-    speaker.name = "Test, Test"
-    speaker.email = "invalid_email!com"
-    assert_not speaker.save, "Saved the speaker with an invalid email"
+
+  it "is not valid with an invalid email" do
+    speaker = Speaker.new(name: "Test, Test", email: "invalid_email!com")
+    expect(speaker.save).to be_falsey, "Saved the speaker with an invalid email"
   end
-  test "should save speaker with valid name and email" do
-    speaker = Speaker.new
-    speaker.name = "Andy, Test"
-    speaker.email = "andycorrales7@gmail.com"
-    assert speaker.save, "Did not save the speaker with a valid name and email"
+
+  it "is valid with a name and a valid email" do
+    speaker = Speaker.new(name: "Andy, Test", email: "andycorrales7@gmail.com")
+    expect(speaker.save).to be_truthy, "Did not save the speaker with a valid name and email"
+  end
 end
